60bccae94d774b7378bc6b372b9500d8ddd265eb
Joshua Siagian <suabahasa@gmail.com>
2021-10-08 07:07:55 +0700

    downgradeExpiredQueryBuilder

--- src/Repository/BillingRepository.php
+++ src/Repository/BillingRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Billing;
+use Carbon\CarbonImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,6 +16,9 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class BillingRepository extends ServiceEntityRepository
 {
+    private const DAYS_GRACE_PERIOD = 3;
+    private const DAYS_EXPIRE_REMINDER = 7;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Billing::class);
@@ -47,4 +52,73 @@ class BillingRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    public function countDowngradeExpired(): int
+    {
+        return $this->getExpiredQueryBuilder()->select('COUNT(b.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    public function downgradeExpired(): int
+    {
+        $qb = $this->getExpiredQueryBuilder();
+
+        $parameters = $qb->getParameters()->toArray();
+        
+        return $qb->update()
+            ->set('b.isActive', ':is_active')
+            ->set('b.isAutoRenewal', ':is_auto_renewal')
+            ->set('b.expiredAt', ':expired_at')
+            ->setParameters(array_merge(
+                array_combine(
+                    array_map(fn ($element) => $element->getName(), $parameters),
+                    array_map(fn ($element) => $element->getValue(), $parameters)
+                ),
+                [
+                    'is_active' => false,
+                    'is_auto_renewal' => false,
+                    'expired_at' => null,
+                ]
+            ))
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
+    private function getExpiredQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.isActive = :where_is_active')
+            ->andWhere('b.expiredAt IS NOT NULL')
+            ->andWhere('b.expiredAt < :graced_date')
+            ->setParameters([
+                'where_is_active' => true,
+                'graced_date' => CarbonImmutable::now()->subDays(self::DAYS_GRACE_PERIOD)
+            ]);
+    }
+
+    private function getGracedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.isActive = :where_is_active')
+            ->andWhere('b.expiredAt IS NOT NULL')
+            ->andWhere('b.expiredAt < :due_date')
+            ->andWhere('b.expiredAt > :graced_date')
+            ->setParameters([
+                'where_is_active' => true,
+                'due_date' => CarbonImmutable::now(),
+                'graced_date' => CarbonImmutable::now()->subDays(self::DAYS_GRACE_PERIOD)
+            ]);
+    }
+
+    private function getReminderQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.isActive = :where_is_active')
+            ->andWhere('b.expiredAt IS NOT NULL')
+            ->andWhere('b.expiredAt < :reminder_date')
+            ->setParameters([
+                'where_is_active' => true,
+                'reminder_date' => CarbonImmutable::now()->addDays(self::DAYS_EXPIRE_REMINDER),
+            ]);
+    }
 }
