9b0fe05bcac61624ec3944d549569c4daa847f48
Joshua Siagian <suabahasa@gmail.com>
2021-11-09 10:21:53 +0700

    update ecs config

--- ecs.php
+++ ecs.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
