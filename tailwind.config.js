e7114db493fc95a8cd4eafc8b8b3dbe0e312f496
dplugins-dev <mail@dplugins.com>
2022-02-11 15:16:03 +0000

    chore: upgrade npm package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,16 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './templates/**/*.html.twig',
     './assets/**/*.{js,jsx,ts,tsx,vue}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: colors.cyan,
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
